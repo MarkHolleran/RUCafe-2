1981056ba2539eeaa646f8578ce8afbd3a4937bd
Abhitej Bokka <abhitej.bokka@gmail.com>
2022-04-20 18:03:30 -0400

    a

--- app/src/main/java/com/rutgers/rucafe/donut_order.java
+++ app/src/main/java/com/rutgers/rucafe/donut_order.java
@@ -1,14 +1,19 @@
 package com.rutgers.rucafe;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Button;
 
 import android.os.Bundle;
 
 public class donut_order extends AppCompatActivity {
 
+    private Button addButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donut_order);
+
+        addButton = findViewById(R.id.btn_add);
     }
 }
