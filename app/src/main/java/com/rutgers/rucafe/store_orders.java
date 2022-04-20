eb815b1b1880426a065adc1fcfa8f1ce64dc299a
MarkHolleran <97909762+MarkHolleran@users.noreply.github.com>
2022-04-20 12:48:41 -0400

    added the 4 activity files we'll need to create the buttons and layout for

---
+++ app/src/main/java/com/rutgers/rucafe/store_orders.java
@@ -0,0 +1,14 @@
+package com.rutgers.rucafe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class store_orders extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_store_orders);
+    }
+}
