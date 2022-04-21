716e80046836c441d0857211a4c3056bdc5987a4
MarkHolleran <97909762+MarkHolleran@users.noreply.github.com>
2022-04-21 00:52:26 -0400

    i hate java

--- app/src/main/java/com/rutgers/rucafe/MainActivity.java
+++ app/src/main/java/com/rutgers/rucafe/MainActivity.java
@@ -2,15 +2,31 @@ package com.rutgers.rucafe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        this.button = findViewById(R.id.OrderDonutButton);
+        button.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+
+                startActivity(new Intent(MainActivity.this, DonutActivity.class));
+
+            }
+        });
+
+    }
 }
